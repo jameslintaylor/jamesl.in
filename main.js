3b7fc60003e7349b73bd455b67ee0014a35bae67
jlt <jlt@jlt2.local>
2017-02-22 21:07:05 -0500

    test

--- main.js
+++ main.js
@@ -3,17 +3,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-type Props = {
-	title: string,
-	valid: boolean
-}
-
-function App(props: Props) {
-	return <h>hi mom</h>
+function App(props) {
+	return (
+		<div>
+			<img src="static/mewtwo.png" />
+			<h2>jlt</h2>
+		</div>
+	)
 }
 
 ReactDOM.render(
-	<App title="cool" valid={true}/>,
+	<App />,
 	document.getElementById('root')
 );
 
+function test() {
+	let anInt: number = 2;
+	let aString: string = "abcdef";
+	let x: boolean = anInt + aString;
+}
